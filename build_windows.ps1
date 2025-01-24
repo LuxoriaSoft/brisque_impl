2ff6066bf88798e80ac1bd7810191c9dbd74e066
QuentinCraft <91551241+QuentinCraft@users.noreply.github.com>
2025-01-24 12:35:31 +0000

    feat: refactor Windows build workflow to use matrix strategy and streamline Tesseract installation

--- build_windows.ps1
+++ build_windows.ps1
@@ -1,15 +1,16 @@
 function BuildForWindows($targetPlatform, $vcpkgPath, $runMsbuild, $hostPlatform) {  
+    # Set default host platform if not provided
     if (-not $hostPlatform) {
         $hostPlatform = "x64"
     }
 
-    #$ErrorActionPreference = "Stop"
-
+    # Set build directory
     $buildDirectory = "build_win_${targetPlatform}"
     mkdir $buildDirectory -Force -ErrorAction Stop | Out-Null
     cd $buildDirectory
-    pwd
+    Write-Host "Building in directory: $(pwd)"
 
+    # Determine platform-specific settings
     if ($targetPlatform -eq "x64") {
         $targetProcessor = "AMD64"
         $msbuildPlatform = "x64"
@@ -25,10 +26,11 @@ function BuildForWindows($targetPlatform, $vcpkgPath, $runMsbuild, $hostPlatform
     else {
         $targetProcessor = "x86"
         $msbuildPlatform = "Win32"
-        $msmfFlag = "OFF" # opencv_videoio430.lib(cap_msmf.obj) : error LNK2001: unresolved external symbol _MFVideoFormat_H263
+        $msmfFlag = "OFF" # MSMF not supported on x86
         $caroteneFlag = "ON"
     }
 
+    # Determine host processor
     if ($hostPlatform -eq "x64") {
         $hostProcessor = "AMD64"
     } elseif ($hostPlatform -eq "arm64") {
@@ -37,13 +39,13 @@ function BuildForWindows($targetPlatform, $vcpkgPath, $runMsbuild, $hostPlatform
         $hostProcessor = "x86"
     }
 
+    # Handle cross-compiling
     $crossCompileOptions = ""
     if ($hostPlatform -ne $targetPlatform) {
-        # Based on https://github.com/opencv/opencv/issues/24235#issuecomment-1713781654 and https://github.com/opencv/opencv/issues/24235#issuecomment-1719639169
-
         $crossCompileOptions = "-D CMAKE_CROSSCOMPILING=1"
     }
 
+    # Run CMake configuration
     cmake -G "Visual Studio 17 2022" `
         -A $msbuildPlatform `
         $crossCompileOptions `
@@ -57,61 +59,43 @@ function BuildForWindows($targetPlatform, $vcpkgPath, $runMsbuild, $hostPlatform
         -D INSTALL_PYTHON_EXAMPLES=OFF `
         -D BUILD_DOCS=OFF `
         -D BUILD_WITH_DEBUG_INFO=OFF `
-        -D BUILD_DOCS=OFF `
         -D BUILD_EXAMPLES=OFF `
         -D BUILD_TESTS=OFF `
         -D BUILD_PERF_TESTS=OFF `
         -D BUILD_JAVA=OFF `
-        -D BUILD_WITH_DEBUG_INFO=OFF `
-        -D BUILD_opencv_apps=OFF `
-        -D BUILD_opencv_datasets=OFF `
-        -D BUILD_opencv_gapi=OFF `
-        -D BUILD_opencv_java_bindings_generator=OFF `
-        -D BUILD_opencv_js=OFF `
-        -D BUILD_opencv_js_bindings_generator=OFF `
-        -D BUILD_opencv_objc_bindings_generator=OFF `
-        -D BUILD_opencv_python_bindings_generator=OFF `
-        -D BUILD_opencv_python_tests=OFF `
-        -D BUILD_opencv_ts=OFF `
         -D BUILD_opencv_world=OFF `
         -D WITH_MSMF=${msmfFlag} `
-        -D WITH_MSMF_DXVA=${msmfFlag} `
         -D WITH_QT=OFF `
-        -D WITH_FREETYPE=OFF `
         -D WITH_TESSERACT=ON `
-        -D WITH_CAROTENE=${caroteneFlag} `
         -D Tesseract_INCLUDE_DIR="${vcpkgPath}/installed/${targetPlatform}-windows-static/include" `
-        -D Tesseract_LIBRARY="${vcpkgPath}/installed/${targetPlatform}-windows-static/lib/tesseract41.lib" `
-        -D Lept_LIBRARY="${vcpkgPath}/installed/${targetPlatform}-windows-static/lib/leptonica-1.81.0.lib" `
+        -D Tesseract_LIBRARY="${vcpkgPath}/installed/${targetPlatform}-windows-static/lib/tesseract.lib" `
+        -D Lept_LIBRARY="${vcpkgPath}/installed/${targetPlatform}-windows-static/lib/leptonica.lib" `
         -D ENABLE_CXX11=1 `
         -D OPENCV_ENABLE_NONFREE=ON `
         -D OPENCV_EXTRA_MODULES_PATH=../libs/opencv_contrib/modules `
         -D BUILD_SHARED_LIBS=OFF ../libs/opencv
-    # ENABLE_CXX11 is for Tesseract (https://github.com/opencv/opencv_contrib/blob/a26f71313009c93d105151094436eecd4a0990ed/modules/text/cmake/init.cmake#L19)
 
+    # Build the project with MSBuild if requested
     if ($runMsbuild) {
-        # Developer Powershell for VS 2019
-        # Path: C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe -noe -c "&{Import-Module """C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"""; Enter-VsDevShell cebe9bd5}"
-        # WorkDir: C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\
-
+        Write-Host "Building with MSBuild for platform: $msbuildPlatform"
         msbuild INSTALL.vcxproj /t:build /p:configuration=Release /p:platform=$msbuildPlatform -maxcpucount
+        Write-Host "Build completed. Listing build output:"
         ls
     }
 
+    # Return to the parent directory
     cd ..
 }
 
-
-# Entry point
-If ((Resolve-Path -Path $MyInvocation.InvocationName).ProviderPath -eq $MyInvocation.MyCommand.Path) {
-
-    ##### Change here #####
+# Entry Point
+if ((Resolve-Path -Path $MyInvocation.InvocationName).ProviderPath -eq $MyInvocation.MyCommand.Path) {
+    # Target platform for the build
     $platform = "x64"
-    #$platform = "x86"
-    #$platform = "arm64"
 
+    # Ensure VCPKG dependencies are installed
+    Write-Host "Installing Tesseract dependency with vcpkg for platform: $platform"
     Invoke-Expression "vcpkg install tesseract:${platform}-windows-static" -ErrorAction Stop
-    #Invoke-Expression "${vcpkgPath}\vcpkg.exe integrate install" -ErrorAction Stop
 
-    BuildForWindows $platform $vcpkgPath $FALSE
+    # Start the build process
+    BuildForWindows $platform $Env:VCPKG_ROOT $FALSE
 }
